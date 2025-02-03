27fd3e452cf12a70e2fd205de2eb42f207d4f144
Manal Magdy <nitteam2024@gmail.com>
2025-02-03 09:01:22 +0000

    updating ads in sidebar

--- app/[locale]/about-app/page.tsx
+++ app/[locale]/about-app/page.tsx
@@ -1,18 +1,38 @@
 import AboutDataDetails from '@/components/about-app/AboutDataDetails';
-import { fetchData } from '@/lib/fetchData'
-import React from 'react'
+import React from 'react';
 
-const page = async () => {
-    const { data, error, loading } = await fetchData('/api/about-app', { cache: "no-store" })
+const page = async ({ searchParams }: { searchParams: any }) => {
+    let loading = false;
+    const fetchData = async () => {
+        try {
+            loading = true;
+            const res = await fetch(`${process.env.BASE_URL}/api/about-app`, {
+                method: "GET",
+                credentials: "include",
+                cache: "no-store"
+            });
+            if (!res.ok) {
+                return { data: null, error: await res.text() };
+            }
+            const data = await res.json();
+            return { data: data, error: null };
+        } catch (error: any) {
+            return { data: null, error: error?.message };
+        } finally {
+            loading = false;
+        }
+    };
 
-    if (loading) return <div>Loading...</div>
+    const { data, error } = await fetchData();
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
 
     return (
         <div className='p-container space-y-10 pb-5'>
-            {/* <AboutDataDetails aboutApp={data.aboutApp} /> */}
-            about us
+            <AboutDataDetails aboutApp={data?.aboutApp} />
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default page;
