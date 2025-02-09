19736a2191b6660221c91d4150b5808812fc75e9
Manal Magdy <nitteam2024@gmail.com>
2025-02-09 09:24:45 +0000

    refactor styles and improve layout in globals.css and appContext.tsx

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -1,38 +1,43 @@
-'use client'
-import React, { createContext, useContext, useState } from 'react';
-import Login from '@/components/Login/Login';
-import axios from 'axios';
-import Sidebar from '@/components/Sidebar';
-import Navbar from '@/components/Navbar';
+"use client";
+import React, { createContext, useContext, useState } from "react";
+import Login from "@/components/Login/Login";
+import axios from "axios";
+import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
 
 type TypeContext = {
-    user: any,
-    token: string;
+  user: any;
+  token: string;
 };
 
-const ProviderContext = createContext<TypeContext>({ user: null, token: '' });
+const ProviderContext = createContext<TypeContext>({ user: null, token: "" });
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL as string;
 axios.defaults.withCredentials = true;
 
-const ProvdierApp = ({ children, user, token }: { children: React.ReactNode, user: any, token: string }) => {
-    const [open, setOpen] = useState(false)
-    return (
-        <ProviderContext.Provider value={{ user, token }}>
-            <div className='flex'>
-                <Sidebar
-                    open={open}
-                    setOpen={setOpen}
-                />
-                <div className='flex-1 w-full lg:w-[calc(100%-18rem)] bg-[#f0f2f5]'>
-                    <Navbar setOpen={setOpen} />
-                    {children}
-                </div>
-            </div>
-        </ProviderContext.Provider>
-    );
+const ProvdierApp = ({
+  children,
+  user,
+  token,
+}: {
+  children: React.ReactNode;
+  user: any;
+  token: string;
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <ProviderContext.Provider value={{ user, token }}>
+      <div className="flex">
+        <Sidebar open={open} setOpen={setOpen} />
+        <div className="flex-1 content-container">
+          <Navbar setOpen={setOpen} />
+          {children}
+        </div>
+      </div>
+    </ProviderContext.Provider>
+  );
 };
 
 export const useAppContext = () => {
-    return useContext(ProviderContext)
-}
+  return useContext(ProviderContext);
+};
 export default ProvdierApp;
