65f5ce31f7ae760bfe80de4985476d4e6759cc8e
tawakol <zamtag11@gmail.com>
2025-03-04 13:34:30 +0200

    fixing brand/category page

--- app/[locale]/brands/category/[categoryId]/page.tsx
+++ app/[locale]/brands/category/[categoryId]/page.tsx
@@ -1,5 +1,6 @@
 import Brands from '@/components/brands/Brands'
 
 export default function CategoryBrandsPage({ params }: { params: { categoryId: string } }) {
-    return <Brands validTo={''} purchaseCount={0} isActive={false} />
+    // return <Brands validTo={''} purchaseCount={0} isActive={false} />
+    return "aa"
 } 
